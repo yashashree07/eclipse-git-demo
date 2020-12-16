7aa7e25136a5fba579f8143db86029351073841b
yashashree07 <suryawanshiyashashree@gmail.com>
2020-12-16 18:13:23 +0530

    initial commit

---
+++ SampleJavaProj/src/AddClass.java
@@ -0,0 +1,13 @@
+
+public class AddClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=2,n2=3;
+		int result=n1+n2;
+		
+		System.out.println("Result : "+result);
+
+	}
+
+}
